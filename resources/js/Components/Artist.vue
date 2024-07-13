eabdad68665e038097eaa971e76fc2c7ef3116f3
Farzad Hayatbakhsh <farzad.hayatbakhsh@tiny.cloud>
2024-07-13 19:03:14 +1000

    Add Artist/List and Artist/Show pages with working routing

---
+++ resources/js/Components/Artist.vue
@@ -0,0 +1,10 @@
+<script setup>
+    const props = defineProps(['artist']);
+</script>
+
+<template>
+    <h1><b>Name: </b>Hello, {{ artist.name }}!</h1>
+    <p><b>Bio: </b>{{ artist.bio }}</p>
+    <p><b>Location: </b>{{ artist.location }}</p>
+    <br>
+</template>
