f6b3dbdaff6ca8174f76a4055c5d45c99e8dadfc
ArachnidsGrip <minervavine@gmail.com>
2024-07-13 18:33:44 +1000

    Event Controller

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -49,10 +49,77 @@ class EventController extends Controller
             'end' => ['required', 'date'],
             'venue_id' => ['required', 'numeric']
         ]);
+
+        $event = Event::create([
+            'tenant_id' => 0,
+            'name' => $request->name,
+            'venue_id' => $request->venue_id,
+            'start' => $request->start,
+            'end' => $request->end,
+            'description' => $request->description,
+            'ticketing_provider' => $request->ticketing_provider,
+            'free' => $request->free,
+            'all_ages' => $request->all_ages,
+            'pic_url' => $request->pic_url,
+            'ticket_url' => $request->ticket_url,
+            'location' => $request->location,
+            'state' => 'active'
+        ]);
+
+        $event_stage = Event_Stage::create([
+            'tenant_id' => 0,
+            'event_id' => $event->id,
+            'venue_id' => $request->venue_id,
+            'name' => 'Main Stage',
+            'state' => 'active',
+        ]);
     }
 
-    public function updateEvent(Request $request){
-        //
+    public function addSet(Request $request, $event, $stage){
+        $set = Event_Set::create([
+            'tenant_id' => 0,
+            'event_id' => $event->id,
+            'venue_id' => $request->venue_id,
+            'event_stage_id' => $request->stage_id,
+            'artist_id' => $request->artist_id,
+            'name' => $request->name,
+            'time' => $request->time,
+            'duration' => $request->duration,
+        ]);
+    }
+
+    public function removeSet($id){
+        $set = Event_Set::find($id);
+        $set->delete();
+    }
+
+    public function updateEvent(Request $request, $id){
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'start' => ['required', 'date'],
+            'end' => ['required', 'date'],
+            'venue_id' => ['required', 'numeric']
+        ]);
+
+        $event = Event::find($id);
+        if ($event->venue_id !== $request->venue_id) {
+            $stage = Event_Stage::where('event_id', $id)->first(); //TODO: Multi-stage needs to go through each stage and reset the venue id
+            $stage->venue_id = $request->venue_id;
+            $stage->save();
+        }
+
+        $event->name = $request->name;
+        $event->venue_id = $request->venue_id;
+        $event->start = $request->start;
+        $event->end = $request->end;
+        $event->description = $request->description;
+        $event->ticketing_provider = $request->ticketing_provider;
+        $event->free = $request->free;
+        $event->all_ages = $request->all_ages;
+        $event->pic_url = $request->pic_url;
+        $event->ticket_url = $request->ticket_url;
+        $event->location = $request->location;
+        $event->state = $request->status;
     }
 
     public function destroyEvent($id){
