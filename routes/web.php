8c4e52283dd8526866e5d5e4ea7c9bc3b95fee60
ArachnidsGrip <minervavine@gmail.com>
2024-07-13 23:35:35 +1000

    Routes for get/post

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\ArtistController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\VenueController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -24,4 +26,16 @@ Route::middleware([
     })->name('dashboard');
     Route::get('/artist/{id}', [ArtistController::class, 'getArtist']);
     Route::get('/artists/{pagenum?}', [ArtistController::class, 'getArtistList']);
+    Route::get('/event/{id}', [EventController::class, 'getEvent']);
+    Route::get('/events/{pagenum?}', [EventController::class, 'getEventList']);
+    Route::get('/venue/{id}', [VenueController::class, 'getVenue']);
+    Route::get('/venues/{pagenum?}', [VenueController::class, 'getVenueList']);
+
+    Route::post('/artist/create', [ArtistController::class, 'createArtist']);
+    Route::post('/event/create', [EventController::class, 'createEvent']);
+    Route::post('/venue/create', [VenueController::class, 'createVenue']);
+    Route::post('/artist/update', [ArtistController::class, 'updateArtist']);
+    Route::post('/event/update', [EventController::class, 'updateEvent']);
+    Route::post('/venue/update', [VenueController::class, 'updateVenue']);
+    Route::post('/event/createSet', [EventController::class, 'createSet']);
 });
