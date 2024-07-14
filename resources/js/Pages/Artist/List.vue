e12a7574a986ad9397d990152d3ca4d1e01f57fb
Farzad Hayatbakhsh <farzadhayat2002@gmail.com>
2024-07-14 13:11:58 +1000

    Add Artist pages into AppLayout

--- resources/js/Pages/Artist/List.vue
+++ resources/js/Pages/Artist/List.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { Link } from '@inertiajs/vue3'
+import AppLayout from '@/Layouts/AppLayout.vue';
+import { Link } from '@inertiajs/vue3';
 
 const props = defineProps(['artists', 'count']);
 
@@ -7,36 +8,33 @@ const numberOfPages = Math.ceil(props.count / 10);
 </script>
 
 <template>
-    <div class="overflow-x-auto m-10">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Location</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="artist in artists" :key="artist.id">
-                    <td class="link link-primary">
-                        <a :href="`/artist/${artist.id}`">{{ artist.name }}</a>
-                    </td>
-                    <td>{{ artist.email }}</td>
-                    <td>{{ artist.location }}</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <div class="join flex justify-center fixed bottom-0 w-full p-4">
-        <Link
-            v-for="page in numberOfPages"
-            :key="page"
-            class="join-item btn"
-            :href="`/artists/${page - 1}`"
-            as="button"
-            type="button"
-        >
+    <AppLayout title="Artists">
+        <h1 class="text-2xl font-semibold text-center pt-10">Artists</h1>
+        <div class="overflow-x-auto m-10">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Location</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="artist in artists" :key="artist.id">
+                        <td class="link link-primary">
+                            <Link :href="`/artist/${artist.id}`">{{ artist.name }}</Link>
+                        </td>
+                        <td>{{ artist.email }}</td>
+                        <td>{{ artist.location }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="join flex justify-center fixed bottom-0 w-full p-4">
+            <Link v-for="page in numberOfPages" :key="page" class="join-item btn" :href="`/artists/${page - 1}`"
+                as="button" type="button">
             {{ page }}
-        </Link>
-    </div>
+            </Link>
+        </div>
+    </AppLayout>
 </template>
--- resources/js/Pages/Artist/Show.vue
+++ resources/js/Pages/Artist/Show.vue
@@ -1,9 +1,24 @@
 <script setup>
-    import Artist from '@/Components/Artist.vue';
+import AppLayout from '@/Layouts/AppLayout.vue';
 
-    const props = defineProps(['artist']);
+const props = defineProps(['artist']);
 </script>
 
 <template>
-    <Artist :artist="artist" />
+    <AppLayout title="Artist Profile">
+        <div class="card card-side bg-base-100 shadow-xl border m-10 max-w-screen">
+            <div class="card-body">
+                <h2 class="card-title">{{ artist.name }}</h2>
+                <p class="text-sm text-gray-500">{{ artist.email }}</p>
+                <p>{{ artist.bio }}</p>
+                <div class="mt-4 text-sm">
+                    <span class="font-semibold">Location: </span>{{ artist.location }}
+                </div>
+            </div>
+            <figure class="aspect-w-1 aspect-h-1">
+                <img :src="artist.pic_url || 'https://i.pravatar.cc/300'" :alt="`${artist.name} Profile Picture`"
+                    class="h-full object-cover aspect-ratio" />
+            </figure>
+        </div>
+    </AppLayout>
 </template>
