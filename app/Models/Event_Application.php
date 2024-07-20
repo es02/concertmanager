19125215b2e2ff84f661eb852d0c11190772e7b5
ArachnidsGrip <minervavine@gmail.com>
2024-07-20 11:59:48 +1000

    Add new models

---
+++ app/Models/Event_Application.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event_Application extends Model
+{
+    use HasFactory;
+
+    protected $table = 'event_application';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'tenant_id',
+        'event_id',
+        'name',
+        'type',
+        'published',
+        'state',
+    ];
+}
