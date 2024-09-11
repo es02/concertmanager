8f889d5c1368dbc3af46772a191973d2f51df0f4
ArachnidsGrip <minervavine@gmail.com>
2024-09-11 19:56:31 +1000

    Finish entry options and form submission

--- resources/js/Pages/Apply/Create.vue
+++ resources/js/Pages/Apply/Create.vue
@@ -36,7 +36,7 @@ var defaultEntry = {
     entryDescription: '',
     entryMappedField: '',
     entryMandatory: false,
-    entryOptions: {},
+    entryOptions: [],
 };
 
 var defaultImage = {
@@ -109,6 +109,7 @@ function updateEntry(id, entry){
         form.entries[id].entryName = entry.entryName;
         form.entries[id].entryDescription = entry.entryDescription;
         form.entries[id].entryMappedField = entry.entryMappedField;
+        form.entries[id].entryOptions = entry.entryOptions;
     }
 }
 
@@ -161,7 +162,7 @@ const createForm = () => {
                 <button @click="newImage()" type="button" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
                     New Image
                 </button>
-                <PrimaryButton type="button" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
+                <PrimaryButton @click="createForm()" type="button" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
                     <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M14.707 7.793a1 1 0 0 0-1.414 0L11 10.086V1.5a1 1 0 0 0-2 0v8.586L6.707 7.793a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.416 0l4-4a1 1 0 0 0-.002-1.414Z"/>
                         <path d="M18 12h-2.55l-2.975 2.975a3.5 3.5 0 0 1-4.95 0L4.55 12H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2Zm-3 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"/>
