49e9adaedf01cde4d16ee7d902deeed15f648eca
ArachnidsGrip <minervavine@gmail.com>
2024-09-27 02:01:52 +1000

    Star rating component

---
+++ resources/js/Components/Rating.vue
@@ -0,0 +1,21 @@
+<script setup>
+import FilledStar from '@/Components/Icons/FilledStar.vue';
+import UnFilledStar from '@/Components/Icons/UnFilledStar.vue';
+
+defineProps(['rating']);
+</script>
+
+<template>
+    <div class="flex items-center">
+        <FilledStar v-if="rating > 0"></FilledStar>
+        <UnFilledStar v-else></UnFilledStar>
+        <FilledStar v-if="rating > 1"></FilledStar>
+        <UnFilledStar v-else></UnFilledStar>
+        <FilledStar v-if="rating > 2"></FilledStar>
+        <UnFilledStar v-else></UnFilledStar>
+        <FilledStar v-if="rating > 3"></FilledStar>
+        <UnFilledStar v-else></UnFilledStar>
+        <FilledStar v-if="rating > 4"></FilledStar>
+        <UnFilledStar v-else></UnFilledStar>
+    </div>
+</template>
