4d444259b0c47aae57b0274bb2af7bec03c19c2b
ArachnidsGrip <minervavine@gmail.com>
2024-10-22 17:54:36 +1000

    fix 500 on null fee

--- resources/js/Pages/Event/ApplicationList.vue
+++ resources/js/Pages/Event/ApplicationList.vue
@@ -182,7 +182,7 @@ function sort(sort) {
                         </td>
                         <td>{{ application.location }}</td>
                         <td>{{ application.genre }}</td>
-                        <td v-html="application.fee.slice(0,40)"></td>
+                        <td v-if="application.fee !== null"  v-html="application.fee.slice(0,40)"></td>
                         <td><Rating :rating="application.rating"></Rating></td>
                         <td>
                             <span v-if="application.new" class="badge badge-neutral gap-2">New</span>
