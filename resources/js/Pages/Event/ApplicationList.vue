7a99dda4adf855d76673eaf9f9760c1ca26faa00
ArachnidsGrip <minervavine@gmail.com>
2025-01-21 19:10:59 +1000

    Export application CSV

--- resources/js/Pages/Event/ApplicationList.vue
+++ resources/js/Pages/Event/ApplicationList.vue
@@ -136,6 +136,10 @@ function sort(sort) {
         router.get(`/event/applications/${eventID}/sort/${sort}/${currentPage.value}`);
     }
 }
+
+function downloadCSV() {
+    window.location.href = `/applications/${eventID}/export-csv`;
+};
 </script>
 
 <template>
@@ -145,7 +149,8 @@ function sort(sort) {
             <div class="overflow-x-auto m-10 text-right grid grid-cols-4">
                 <p></p>
                 <p></p>
-                <p></p>
+                <p>
+                    <button type="submit" @click.prevent="downloadCSV()" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Export CSV</button></p>
                 <p>
                     <select
                         id="filter"
